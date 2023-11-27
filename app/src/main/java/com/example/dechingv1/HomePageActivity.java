508c0e139469b9d79b69f7f41f286ce0cbcc7a73
unknown <darofaalll@gmail.com>
2023-11-27 09:42:10 +0100

    Refonte des pages

--- app/src/main/java/com/example/dechingv1/HomePageActivity.java
+++ app/src/main/java/com/example/dechingv1/HomePageActivity.java
@@ -2,13 +2,25 @@ package com.example.dechingv1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomePageActivity extends AppCompatActivity {
-
+    private ImageButton boutonLogo,boutonHome,boutonMap,boutonAddPost,boutonEvent,boutonProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        boutonMap=(ImageButton)findViewById(R.id.imageButtonMap);
+        boutonMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMap=new Intent(HomePageActivity.this, MapActivity.class);
+                startActivity(intentMap);
+            }
+        });
     }
 }
