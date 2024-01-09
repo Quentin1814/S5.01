43332a84bf68b796a38650537d1e78827636fd83
Esteban Dujardin <este22pro@gmail.com>
2024-01-09 11:48:48 +0100

    Avancement Connexion et Création de compte d'un utilisateur à grâce à une API de connexion à une base de données locale

--- app/src/main/java/com/example/dechingv1/CreateAccountActivity.java
+++ app/src/main/java/com/example/dechingv1/CreateAccountActivity.java
@@ -8,6 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -20,6 +32,7 @@ public class CreateAccountActivity extends AppCompatActivity {
 
     private String username;
     private String password;
+    private DatabaseManager databaseManager;
 
 
     @Override
@@ -33,13 +46,16 @@ public class CreateAccountActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.createPasswordEditText);
         errorCreateAccountTextView = findViewById(R.id.errorCreateAccountTextView);
 
+        databaseManager = new DatabaseManager(getApplicationContext());
+
         createAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 username = usernameEditText.getText().toString();
                 password = passwordEditText.getText().toString();
 
-                //Lancer la requête pour connecter l'utilisateur
+                createAccount();
+                //Lancer la requête pour inscrire l'utilisateur
             }
         });
 
@@ -50,5 +66,52 @@ public class CreateAccountActivity extends AppCompatActivity {
                 startActivity(createToAccountActivity);
             }
         });
+    }
+
+    public void onApiResponse(JSONObject response) {
+        Boolean success = null;
+        String error = "";
+
+        try {
+            success = response.getBoolean("success");
+
+            if (success == true) {
+                Intent interfaceActivity = new Intent(getApplicationContext(), InterfaceActivity.class);
+                interfaceActivity.putExtra("username", username);
+                startActivity(interfaceActivity);
+                finish();
+            } else {
+                error = response.getString("error");
+                errorCreateAccountTextView.setVisibility(View.VISIBLE);
+                errorCreateAccountTextView.setText(error);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void createAccount() {
+        String url = "http://10.0.2.2/test/actions/createAccount.php";
+
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+        JSONObject parameters = new JSONObject(params);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, parameters, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                onApiResponse(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        databaseManager.queue.add(jsonObjectRequest);
     }
 }
--- app/src/main/java/com/example/dechingv1/MainActivity.java
+++ app/src/main/java/com/example/dechingv1/MainActivity.java
@@ -8,6 +8,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.JsonObject;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     private String username;
     private String password;
+    private DatabaseManager databaseManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +46,15 @@ public class MainActivity extends AppCompatActivity {
         connectBtn = findViewById(R.id.connectBtn);
         createAccountBtn = findViewById(R.id.createAccountBtn);
 
+        databaseManager = new DatabaseManager(getApplicationContext());
+
         connectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 username = usernameEditText.getText().toString();
                 password = passwordEditText.getText().toString();
+
+                connectUser();
 
                 //Lancer la requête pour connecter l'utilisateur
             }
@@ -47,7 +65,57 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent createAccountActivity = new Intent(getApplicationContext(), CreateAccountActivity.class);
                 startActivity(createAccountActivity);
+                finish();
             }
         });
+    }
+
+    public void onApiResponse(JSONObject response) {
+        Boolean success = null;
+        String error = "";
+
+        try {
+            success = response.getBoolean("success");
+
+            if (success == true) {
+                Intent interfaceActivity = new Intent(getApplicationContext(), InterfaceActivity.class);
+                interfaceActivity.putExtra("username", username);
+                startActivity(interfaceActivity);
+                finish();
+            } else {
+                error = response.getString("error");
+                errorConnectAccountTextView.setVisibility(View.VISIBLE);
+                errorConnectAccountTextView.setText(error);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void connectUser() {
+        // 10.0.2.2 correspond à localhost dans google chrome
+        String url = "http://10.0.2.2/Test/actions/connectUser.php";
+
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+        JSONObject parameters = new JSONObject(params);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, parameters, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                onApiResponse(response);
+                Toast.makeText(getApplicationContext(), "OPERATION SUCCESSFUL", Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        databaseManager.queue.add(jsonObjectRequest);
     }
 }
